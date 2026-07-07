733e9cc1c5d5608ce1e6c8fb5a58e0a098cdda0d
酱牛肉 <jiangniurou.xyf@alibaba-inc.com>
2026-07-07 20:24:40 +0800

    chore: apply black formatting to test files

--- tests/unit/test_json_file_chunker.py
+++ tests/unit/test_json_file_chunker.py
@@ -12,7 +12,6 @@ import pytest
 
 from reme.components.file_chunker import JsonFileChunker
 
-
 # ---------------------------------------------------------------------------
 # Helpers / fixtures
 # ---------------------------------------------------------------------------
